3404866c905c66b2b1e9abffbf1e5e039c56aebd
Robin Bette <robinbette@hotmail.com>
2021-05-13 12:21:27 +0200

    WIP: Extract form

---
+++ src/components/common/useShipmentForm.ts
@@ -0,0 +1,32 @@
+import React, { ChangeEvent, useState } from "react";
+
+const useShipmentForm = () => {
+    // const handleSubmit = (e: any) => {
+    //     e.preventDefault();
+
+    //     console.log("Handle submit");
+    //     console.log("ShipmentPalletDetails = ", shipmentDetails);
+    // };
+
+    // const packageTypes = [
+    //     { value: 1, label: "DoosA4" },
+    //     { value: 2, label: "EuropaPallet" },
+    // ];
+    // const [shipmentDetails, setShipmentDetails] = useState(testShipmentDetailsFullInit);
+
+    // const handleChangePackageType = ({ target: input }: any) => {
+    //     const shipment = { ...shipmentDetails };
+    //     shipment.packageType = input.value;
+    //     resetFormFields(shipment);
+
+    //     setShipmentDetails(shipment);
+    // };
+
+    // const handleChangeShipmentDetails = ({ currentTarget: input }: ChangeEvent<{ name: string; value: unknown }>) => {
+    //     const shipment = { ...shipmentDetails };
+    //     shipment[input.name] = input.value;
+    //     setShipmentDetails(shipment);
+    // };
+}
+
+export default useShipmentForm;
--- src/components/test/testForm.tsx
+++ src/components/test/testForm.tsx
@@ -1,141 +1,106 @@
-import React, { ChangeEvent, useState } from "react";
-import { FormControl, Grid, InputAdornment, TextField, Button, Select, MenuItem } from "@material-ui/core";
-import { resetFormFields, testShipmentDetailsFullInit } from "./models/shipmentDetails";
+import React from "react";
+import { FormControl, Grid, InputAdornment, TextField, Select, MenuItem } from "@material-ui/core";
 
-function TestForm() {
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    console.log("Handle submit");
-    console.log("ShipmentPalletDetails = ", shipmentDetails);
-  };
-
-  const packageTypes = [
-    { value: 1, label: "DoosA4" },
-    { value: 2, label: "EuropaPallet" },
-  ];
-  const [shipmentDetails, setShipmentDetails] = useState(testShipmentDetailsFullInit);
-
-  const handleChangePackageType = ({ target: input }: any) => {
-    const shipment = { ...shipmentDetails };
-    shipment.packageType = input.value;
-    resetFormFields(shipment);
-
-    setShipmentDetails(shipment);
-  };
-
-  const handleChangeShipmentPalletDetails = ({
-    currentTarget: input,
-  }: ChangeEvent<{ name: string; value: unknown }>) => {
-    const shipment = { ...shipmentDetails };
-    shipment[input.name] = input.value;
-    setShipmentDetails(shipment);
-  };
-
+const TestForm = (props: any) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid container>
-        <Grid item xs={5}>
-          <Grid item xs={6}>
-            <FormControl>
-              <Select
-                id="packageType"
-                name="packageType"
-                label="Selecteer pack type"
-                onChange={handleChangePackageType}
-                value={shipmentDetails.packageType}
-                displayEmpty
-                inputProps={{
-                  name: "packingType",
-                }}>
-                <MenuItem value="" disabled />
-                {packageTypes.map((type) => (
-                  <MenuItem key={type.value} value={type.value}>
-                    {type.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <TextField
-                id="oplage"
-                name="oplage"
-                label="Oplage"
-                onChange={handleChangeShipmentPalletDetails}
-                value={shipmentDetails.oplage}
-              />
-            </FormControl>
+    <Grid container>
+      <Grid item xs={5}>
+        <Grid item xs={6}>
+          <FormControl>
+            <Select
+              id="packageType"
+              name="packageType"
+              label="Selecteer pack type"
+              onChange={props.handleChangePackageType}
+              value={props.shipmentDetails.packageType}
+              displayEmpty
+              inputProps={{
+                name: "packingType",
+              }}>
+              <MenuItem value="" disabled />
+              {props.packageTypes.map((type: any) => (
+                <MenuItem key={type.value} value={type.value}>
+                  {type.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <TextField
+              id="oplage"
+              name="oplage"
+              label="Oplage"
+              onChange={props.handleChangeShipmentDetails}
+              value={props.shipmentDetails.oplage}
+            />
+          </FormControl>
 
-            {!shipmentDetails?.packageType ? (
-              <Grid></Grid>
-            ) : (
-              <Grid>
-                {shipmentDetails?.packageType === 1 ? (
-                  <Grid>
-                    <FormControl>
-                      <TextField
-                        id="amountOfItemsPerBox"
-                        name="amountOfItemsPerBox"
-                        label="Aantal items per doos"
-                        onChange={handleChangeShipmentPalletDetails}
-                        value={shipmentDetails.amountOfItemsPerBox}
-                      />
-                    </FormControl>
-                  </Grid>
-                ) : (
-                  <Grid>
-                    <FormControl>
-                      <TextField
-                        id="numberPerPack"
-                        name="numberPerPack"
-                        label="Aantal per pak"
-                        onChange={handleChangeShipmentPalletDetails}
-                        value={shipmentDetails.numberPerPack}
-                      />
-                    </FormControl>
-                    <FormControl>
-                      <TextField
-                        id="numberOfPacksPerLayer"
-                        name="numberOfPacksPerLayer"
-                        label="Aantal pakken per laag"
-                        onChange={handleChangeShipmentPalletDetails}
-                        value={shipmentDetails.numberOfPacksPerLayer}
-                      />
-                    </FormControl>
-                    <FormControl>
-                      <TextField
-                        id="numberOfLayers"
-                        name="numberOfLayers"
-                        label="Aantal lagen"
-                        onChange={handleChangeShipmentPalletDetails}
-                        value={shipmentDetails.numberOfLayers}
-                      />
-                    </FormControl>
-                  </Grid>
-                )}
-              </Grid>
-            )}
+          {!props.shipmentDetails?.packageType ? (
+            <Grid></Grid>
+          ) : (
+            <Grid>
+              {props.shipmentDetails?.packageType === 1 ? (
+                <Grid>
+                  <FormControl>
+                    <TextField
+                      id="amountOfItemsPerBox"
+                      name="amountOfItemsPerBox"
+                      label="Aantal items per doos"
+                      onChange={props.handleChangeShipmentDetails}
+                      value={props.shipmentDetails.amountOfItemsPerBox}
+                    />
+                  </FormControl>
+                </Grid>
+              ) : (
+                <Grid>
+                  <FormControl>
+                    <TextField
+                      id="numberPerPack"
+                      name="numberPerPack"
+                      label="Aantal per pak"
+                      onChange={props.handleChangeShipmentDetails}
+                      value={props.shipmentDetails.numberPerPack}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <TextField
+                      id="numberOfPacksPerLayer"
+                      name="numberOfPacksPerLayer"
+                      label="Aantal pakken per laag"
+                      onChange={props.handleChangeShipmentDetails}
+                      value={props.shipmentDetails.numberOfPacksPerLayer}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <TextField
+                      id="numberOfLayers"
+                      name="numberOfLayers"
+                      label="Aantal lagen"
+                      onChange={props.handleChangeShipmentDetails}
+                      value={props.shipmentDetails.numberOfLayers}
+                    />
+                  </FormControl>
+                </Grid>
+              )}
+            </Grid>
+          )}
 
-            <FormControl>
-              <TextField
-                id="weight"
-                name="weight"
-                label="Gewicht"
-                InputProps={{
-                  endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
-                }}
-                onChange={handleChangeShipmentPalletDetails}
-                value={shipmentDetails.weight}
-              />
-            </FormControl>
-          </Grid>
+          <FormControl>
+            <TextField
+              id="weight"
+              name="weight"
+              label="Gewicht"
+              InputProps={{
+                endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+              }}
+              onChange={props.handleChangeShipmentDetails}
+              value={props.shipmentDetails.weight}
+            />
+          </FormControl>
         </Grid>
       </Grid>
-      <Button type="submit" className="btn btn-primary" variant={"contained"}>
-        Submit
-      </Button>
-    </form>
+    </Grid>
   );
-}
+};
 
 export default TestForm;
