9be1a58c0720be2b5a504c70b808c718ae6ba6c8
Robin Bette <robinbette@hotmail.com>
2021-05-15 12:51:43 +0200

    add validation

--- src/components/login/loginForm.tsx
+++ src/components/login/loginForm.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Login } from "../common/models/login";
+import ValidateLogin from "./validateLogin";
 
 const LoginForm = () => {
   const [login, setLogin] = useState<Login>({ userName: "", password: "", name: "" });
+  const [errors, setErrors] = useState({} as any);
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
@@ -11,19 +13,32 @@ const LoginForm = () => {
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-
-    console.log("Submit login!", login);
+    setErrors(ValidateLogin(login));
   };
+
+  useEffect(() => {
+    // Errors object can change when handleSubmit is clicked.
+    // This effect is fired when errors object changes.
+    // When no error keys exist in the errors object, the console log is fired.
+    if (Object.keys(errors).length > 0) return;
+    console.log("Submit login!", login);
+  }, [errors]);
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>Username</label>
-        <input id="userName" name="userName" type="text" onChange={handleChange} value={login.userName}></input>
+        <input id="userName" name="userName" type="text" onChange={handleChange} value={login.userName} />
+        {errors.userName && <p>{errors.userName}</p>}
+
         <label>Password</label>
-        <input id="password" name="password" type="text" onChange={handleChange} value={login.password}></input>
+        <input id="password" name="password" type="text" onChange={handleChange} value={login.password} />
+        {errors.password && <p>{errors.password}</p>}
+
         <label>Name</label>
-        <input id="name" name="name" type="text" onChange={handleChange} value={login.name}></input>
+        <input id="name" name="name" type="text" onChange={handleChange} value={login.name} />
+        {errors.name && <p>{errors.name}</p>}
+
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
--- src/components/login/validateLogin.ts
+++ src/components/login/validateLogin.ts
@@ -1,3 +1,18 @@
-export const validate = () => {
+import { Login } from "../common/models/login";
 
+const ValidateLogin = (login: Login) => {
+    let errors = {} as any;
+
+    if (!login.userName) {
+        errors.userName = "Username is required"
+    }
+    if (!login.password) {
+        errors.password = "Password is required"
+    }
+    if (!login.name) {
+        errors.name = "Name is required"
+    }
+    return errors;
 }
+
+export default ValidateLogin;
