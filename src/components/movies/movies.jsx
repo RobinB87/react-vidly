004168fe4a643f9cc17748c678b3ca061917fbd7
Robin Bette <robinbette@hotmail.com>
2021-05-16 13:51:14 +0200

    move movies table to get same level of abstraction in movies.jsx file

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { getMovies } from "../../services/fakeMovieService";
 import ListGroup from "./listGroup";
-import Like from "../common/like";
+import MoviesTable from "./moviesTable";
 import Pagination from "../common/pagination";
 import { paginate } from "../../utils/paginate";
 import { getGenres } from "../../services/fakeGenreService";
@@ -68,38 +68,7 @@ class Movies extends Component {
         {/* div col will take up rest of the space */}
         <div className="col">
           <p>There are {numberOfMovies} movies in the database.</p>
-
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like liked={movie.liked} onClick={() => this.handleLike(movie)} />
-                  </td>
-                  <td>
-                    <button onClick={() => this.handleDelete(movie._id)} className="btn btn-danger btn-sm">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
+          <MoviesTable movies={movies} onLike={this.handleLike} onDelete={this.handleDelete} />
           <Pagination
             itemsCount={numberOfMovies}
             pageSize={pageSize}
---
+++ src/components/movies/moviesTable.tsx
@@ -0,0 +1,40 @@
+import Like from "../common/like";
+
+const MoviesTable = (props: any) => {
+  const { movies, onDelete, onLike } = props;
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th />
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie: any) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like liked={movie.liked} onClick={() => onLike(movie)} />
+            </td>
+            <td>
+              <button onClick={() => onDelete(movie._id)} className="btn btn-danger btn-sm">
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
