bb3b5fd023d6a975f20122d77a5015cecb8d1290
Robin Bette <robinbette@hotmail.com>
2021-05-16 15:13:50 +0200

    Small changes

--- src/components/common/tableHeader.tsx
+++ src/components/common/tableHeader.tsx
@@ -12,12 +12,18 @@ const TableHeader = (props: any) => {
     onSort(sortColumnNew);
   };
 
+  const renderSortIcon = (column: any) => {
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return <i className="fa fa-sort-asc"></i>;
+    return <i className="fa fa-sort-desc"></i>;
+  };
+
   return (
     <thead>
       <tr>
         {columns.map((column: any) => (
-          <th key={column.path ?? column.key} onClick={() => raiseSort(column.path)}>
-            {column.label}
+          <th className="clickable" key={column.path ?? column.key} onClick={() => raiseSort(column.path)}>
+            {column.label} {renderSortIcon(column)}
           </th>
         ))}
       </tr>
