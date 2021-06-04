11de80d5db936af32c8d558fd0d8377ef9f11064
Robin Bette <robinbette@hotmail.com>
2021-06-04 09:41:29 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 
+import NavBar from "./components/navBar";
 import Movies from "./components/movies/movies";
 import Customers from "./components/fake/customers";
 import Rentals from "./components/fake/rentals";
@@ -13,20 +14,23 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/login" component={LoginForm} />
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/login" component={LoginForm} />
 
-        <Route path="/movies" component={Movies} />
-        <Route path="/customers" component={Customers} />
-        <Route path="/rentals" component={Rentals} />
-        <Route path="/not-found" component={NotFound} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
 
-        <Route path="/parent" component={ParentForm} />
-        <Redirect from="/" exact to="/movies" component />
-        <Redirect to="not-found" />
-      </Switch>
-    </main>
+          <Route path="/parent" component={ParentForm} />
+          <Redirect from="/" exact to="/movies" component />
+          <Redirect to="not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
